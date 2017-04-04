c7cbf00c161526da7dec087ec9f134b5f5a30b6a
Carmelo Rigoli <crigoli@thebrandagency.co>
2017-04-04 14:17:59 +0800

    Add actions & action tests

---
+++ app/actions/actions.jsx
@@ -0,0 +1,30 @@
+export var setSearchText = (searchText) => {
+    return {
+        type: 'SET_SEARCH_TEXT',
+        searchText
+    };
+};
+
+
+// toggle show completed action generatot
+export var toggleShowCompleted = () => {
+    return {
+        type: 'TOGGLE_SHOW_COMPLETED'
+    };
+};
+
+
+export var addTodo = (text) => {
+    return {
+        type: 'ADD_TODO',
+        text
+    };
+};
+
+// toggleTodo(id) TOGGLE_TODO
+export var toggleTodo = (id) => {
+    return {
+        type: 'TOGGLE_TODO',
+        id
+    };
+};
