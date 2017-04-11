a11048a7b47dc70e2ad747a329a6770b62dad8cc
Carmelo Rigoli <crigoli@thebrandagency.co>
2017-04-11 13:59:34 +0800

    Update todos on firebase

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -40,23 +40,29 @@ describe('Reducers', () => {
             expect(res.length).toEqual(1);
             expect(res[0]).toEqual(action.todo);
         });          
-        it('should toggle todo', () => {
-            var todos = {
-                id: '123',
-                text: 'Something',
-                completed: true,
-                createdAt: 123,
-                completed: 125
-            };    
-            var action = {
-                type: 'TOGGLE_TODO',
-                id: '123'
-            }
-            var res = reducers.todosReducer(df([todos]), df(action));
+        it('should update todo', () => {
+          var todos = [{
+            id: '123',
+            text: 'Something',
+            completed: true,
+            createdAt: 123,
+            completedAt: 125
+          }];
+          var updates = {
+            completed: false,
+            completedAt: null
+          };
+          var action = {
+            type: 'UPDATE_TODO',
+            id: todos[0].id,
+            updates
+          };
+          var res = reducers.todosReducer(df(todos), df(action));
 
-            expect(res[0].completed).toEqual(false);
-            expect(res[0].completedAt).toEqual(undefined);
-        }); 
+          expect(res[0].completed).toEqual(updates.completed);
+          expect(res[0].completedAt).toEqual(updates.completedAt);
+          expect(res[0].text).toEqual(todos[0].text);
+        });
         
         it('should add existing todos', () => {
             var todos = [{
