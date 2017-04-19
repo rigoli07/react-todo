f5db9ebcf3dcf02498fe134bf9921e27ab08d9a8
Carmelo Rigoli <crigoli@thebrandagency.co>
2017-04-19 16:19:22 +0800

    add actions for firebase login and logout

--- app/app.jsx
+++ app/app.jsx
@@ -3,11 +3,12 @@ var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-var TodoApp = require('TodoApp')
 var actions = require('actions');
 //calling configureStore and then grabbing the configure function from it
 var store = require('configureStore').configure(); 
 var TodoAPI = require('TodoAPI');
+import Login from 'Login';
+import TodoApp from 'TodoApp';
 
 //used for listening to changes on the store
 //store.subscribe(() => { 
@@ -31,7 +32,12 @@ require('style!css!sass!applicationStyles')
 ReactDOM.render(
     //add tag so that the todoApp and all of it's children can access the store, as well as disatch actions
 	<Provider store={store}> 
-        <TodoApp/>
+        <Router history={hashHistory}>
+            <Route path="/">
+                <Route path="todos" component={TodoApp}/>
+                <IndexRoute component={Login}/>
+            </Route>
+        </Router>
     </Provider>,
     document.getElementById('app')
 );
